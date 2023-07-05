5a15cad32d7c4e17b1eead549f5b87df3fea7f77
caoao <1745640128@qq.com>
2023-07-05 14:59:55 +0800

    This is the hello world code

---
+++ caonima/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/curry/caonima/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/curry/caonima/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/curry/caonima/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/curry/caonima/src:$ROS_PACKAGE_PATH"
